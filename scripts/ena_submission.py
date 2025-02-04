a974b24b7db4213931ec8a4129f2c7cd28e88409
guillecg <guillemcg92@gmail.com>
2025-02-04 22:05:53 +0100

    Add comment on forward_pattern argument

--- scripts/ena_submission.py
+++ scripts/ena_submission.py
@@ -18,8 +18,9 @@
 # ---------------------------------------------------------------------------- #
 
 # IMPORTANT! Some considerations must be taken into account:
-# Project name is assumed to be the first field in the metadata filename
-# Sample alias is assumed to be the first three fields in the sample filename
+# 1) Project name is assumed to be the first field in the metadata filename
+# 2) Sample alias is assumed to be the first three fields in the sample filename
+# 3) The file pattern for forward reads must contain just one "1"
 
 # ---------------------------------------------------------------------------- #
 
