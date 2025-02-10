4567c3e93b3b17420c6bf841a469e2c30354e7f3
guillecg <guillemcg92@gmail.com>
2025-02-10 12:29:42 +0100

    Move experiment and run XML creation to step 1

--- scripts/step3_register_objects.py
+++ scripts/step3_register_objects.py
@@ -1,10 +1,8 @@
-# ENA submission: THIRD STEP
+# ENA submission: STEP 3
 
 # This script performs the following steps programmatically:
 
-# 3. Experiment and run registration
-# 3.1 Iterate through sample directories for computing checksums
-# 3.2 Register the experiments and runs according to that information
+# 1. Experiment and run registration
 
 # ---------------------------------------------------------------------------- #
 
@@ -26,149 +24,6 @@ import argparse
 from datetime import datetime
 
 import subprocess
-
-
-def create_experiment(
-    samples_receipt_path: str,
-    metadata_path: str,
-    template_dir: str,
-    experiment_type: str
-) -> str:
-
-    # WARNING: project name is assumed to be in the first field of the path
-    project_name = os.path.basename(metadata_path).split("_")[0]
-
-    template_path = os.path.join(
-        template_dir,
-        f"experiment_{experiment_type}.xml"
-    )
-
-    receipt_df = parse_samples_receipt(
-        samples_receipt_path=samples_receipt_path,
-        metadata_path=metadata_path
-    )
-
-    experiment_xml = []
-
-    for _, row in receipt_df.iterrows():
-        row = row.astype(str)
-
-        with open(template_path, mode="r") as handle:
-            template_xml = handle.read()
-
-        exp_alias = f"{project_name}-{row['sample_alias']}-{experiment_type}"
-
-        template_xml = template_xml\
-            .replace("$$$STUDY_ID$$$", row["project_id"])\
-            .replace("$$$EXPERIMENT_ALIAS$$$", exp_alias)\
-            .replace("$$$EXPERIMENT_TITLE$$$", exp_alias)\
-            .replace("$$$SAMPLE_ACCESSION$$$", row["sample_accession"])\
-            .replace("$$$YEAR$$$", str(datetime.now().year))
-
-        experiment_xml += [template_xml]
-
-    experiment_xml = \
-        '<?xml version="1.0" encoding="UTF-8"?>' + "\n" + \
-        "<EXPERIMENT_SET>" + "\n" + \
-        "\n".join(experiment_xml) + "\n" + \
-        "</EXPERIMENT_SET>" + "\n"
-
-    output_path = os.path.join(
-        os.path.dirname(metadata_path),
-        f"{project_name}_ena_experiment_{experiment_type}.xml"
-    )
-    with open(output_path, mode="w") as handle:
-        handle.write(experiment_xml)
-
-    print(f"[+] Experiment XML created: {output_path}")
-
-    return output_path
-
-
-def create_run(
-    samples_dir: str,
-    metadata_path: str,
-    template_dir: str,
-    experiment_type: str,
-    forward_pattern: str
-) -> str:
-
-    # Raise error if samples directory does not exist
-    if not os.path.exists(samples_dir):
-        raise FileNotFoundError(f"{samples_dir} does not exist!")
-
-    # WARNING: project name is assumed to be in the first field of the path
-    project_name = os.path.basename(metadata_path).split("_")[0]
-
-    template_path = os.path.join(
-        template_dir,
-        f"run_{experiment_type}.xml"
-    )
-
-    run_xml = []
-
-    pattern_for = f"{samples_dir}/**/{forward_pattern}"
-
-    for filename_for in glob.glob(pattern_for, recursive=True):
-
-        # Avoid raw reads
-        if "raw" in filename_for:
-            continue
-
-        # Get reverse file from forward one
-        # WARNING: may generate errors there are multiple "1" in the pattern
-        forward_pattern = forward_pattern.replace("*", "")
-        reverse_pattern = forward_pattern.replace("1", "2")
-        filename_rev = filename_for.replace(
-            forward_pattern,
-            reverse_pattern
-        )
-
-        # Raise error when there is not exactly one reverse file
-        if not os.path.exists(filename_rev):
-            raise ValueError(f"[!] Reverse file not found: {filename_rev}")
-
-        # Compute the checksum (MD5)
-        hash_for = hashlib.md5(open(filename_for, mode="rb").read()).hexdigest()
-        hash_rev = hashlib.md5(open(filename_rev, mode="rb").read()).hexdigest()
-
-        with open(template_path, mode="r") as handle:
-            template_xml = handle.read()
-
-        # WARNING: sample alias is assumed to be the first three fields
-        sample_alias = os.path.basename(filename_for)
-        sample_alias = "_".join(sample_alias.split("_")[:3])
-        exp_alias = f"{project_name}-{sample_alias}-{experiment_type}"
-
-        # Add only the filename instead of the whole path
-        filename_for = os.path.basename(filename_for)
-        filename_rev = os.path.basename(filename_rev)
-
-        template_xml = template_xml\
-            .replace("$$$EXPERIMENT_ALIAS$$$",  exp_alias)\
-            .replace("$$$FORWARD_R1_FASTQ$$$",  filename_for)\
-            .replace("$$$FORWARD_R1_MD5SUM$$$", hash_for)\
-            .replace("$$$REVERSE_R2_FASTQ$$$",  filename_rev)\
-            .replace("$$$REVERSE_R2_MD5SUM$$$", hash_rev)
-
-        run_xml += [template_xml]
-
-    run_xml = \
-        '<?xml version="1.0" encoding="UTF-8"?>' + "\n" + \
-        "<RUN_SET>" + "\n" + \
-        "\n".join(run_xml) + "\n" + \
-        "</RUN_SET>" + "\n"
-
-    output_path = os.path.join(
-        os.path.dirname(metadata_path),
-        f"{project_name}_ena_run_{experiment_type}.xml"
-    )
-    with open(output_path, mode="w") as handle:
-        handle.write(run_xml)
-
-    print(f"[+] Run XML created: {output_path}")
-
-    return output_path
 
 
 def register_objects(
@@ -229,16 +84,6 @@ if __name__ == "__main__":
         type=str
     )
     parser.add_argument(
-        "-s", "--samples_dir",
-        help="Directory containing the sequences to submit.",
-        type=str
-    )
-    parser.add_argument(
-        "-r", "--samples_receipt_path",
-        help="XML file returned by ENA after registering the samples.",
-        type=str
-    )
-    parser.add_argument(
         "-t", "--template_dir",
         help="Directory containing the templates for the submission.",
         type=str
@@ -253,28 +98,7 @@ if __name__ == "__main__":
         help="User and password for the submission (e.g. user1:password1234).",
         type=str
     )
-    parser.add_argument(
-        "-f", "--forward_pattern",
-        help="Pattern followed in naming the forward sequence files.",
-        type=str,
-        default="*_1.fastq.gz"
-    )
     args = parser.parse_args()
-
-    experiment_path = create_experiment(
-        samples_receipt_path=args.samples_receipt_path,
-        metadata_path=args.metadata_path,
-        template_dir=args.template_dir,
-        experiment_type=args.experiment_type
-    )
-
-    run_path = create_run(
-        samples_dir=args.samples_dir,
-        metadata_path=args.metadata_path,
-        template_dir=args.template_dir,
-        experiment_type=args.experiment_type,
-        forward_pattern=args.forward_pattern
-    )
 
     final_receipt_path = register_objects(
         metadata_path=args.metadata_path,
