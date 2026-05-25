9ba2e8a9af638c68c0c83ba0b167e1227b183f8c
davidecrs <davidecrs92@gmail.com>
2026-05-25 11:49:05 +0200

    updated s03

--- scripts/s03_create_run_xml.py
+++ scripts/s03_create_run_xml.py
@@ -8,10 +8,9 @@ import sys
 import argparse
 from datetime import datetime
 import pandas as pd
-import bs4 as bs
 import subprocess
 from tqdm import tqdm
-import yaml
+from ruamel.yaml import YAML
 
 
 def main():
@@ -19,26 +18,29 @@ def main():
 
     config_file = args.config_path
     data = read_config(config_file)
-    
-    project_name = data.get("project_name")
-    template_dir = data.get("template_dir")
-    metadata_file = data.get("metadata_file")
-    readmapping_table_wgs = data.get("readmapping_table_wgs")
+
+    project_name              = data.get("project_name")
+    template_dir              = data.get("template_dir")
+    metadata_file             = data.get("metadata_file")
+    readmapping_table_wgs     = data.get("readmapping_table_wgs")
     readmapping_table_amplicon = data.get("readmapping_table_amplicon")
-    raw_data_dir_amplicon = data.get("raw_data_dir_amplicon")
-    raw_data_dir_wgs = data.get("raw_data_dir_wgs")
+    raw_data_dir_amplicon     = data.get("raw_data_dir_amplicon")
+    raw_data_dir_wgs          = data.get("raw_data_dir_wgs")
 
-
-
-    updated_table, table_file = compute_gather(
-        metadata_path= metadata_file,
-        WGS_samples_dir = raw_data_dir_wgs,
-        AMP_samples_dir = raw_data_dir_amplicon,
-        mapping_WGS = readmapping_table_wgs,
-        mapping_AMP = readmapping_table_amplicon,
-        experiment_type=args.experiment_types,
-        nested=args.nested
-    )
+    if args.experiment_types == "16S":
+        updated_table, table_file = compute_gather_amp(
+            AMP_samples_dir=raw_data_dir_amplicon,
+            mapping_AMP=readmapping_table_amplicon,
+            metadata_path=metadata_file,
+            nested=args.nested
+        )
+    elif args.experiment_types == "WGS":
+        updated_table, table_file = compute_gather_wgs(
+            WGS_samples_dir=raw_data_dir_wgs,
+            mapping_WGS=readmapping_table_wgs,
+            metadata_path=metadata_file,
+            nested=args.nested
+        )
 
     mapping_info = updated_table if not updated_table.empty else table_file
 
@@ -46,14 +48,23 @@ def main():
         metadata_path=metadata_file,
         template_dir=template_dir,
         experiment_type=args.experiment_types,
-        project_name = project_name,
+        project_name=project_name,
         mapping=mapping_info,
     )
 
+
 def read_config(config_file: str):
+    yaml = YAML(typ="safe")
+
+    try:
+        with open(config_file, "r") as file:
+            data = yaml.load(file) or {}
+    except FileNotFoundError:
+        # If the file doesn't exist yet, start with a fresh dictionary
+        data = {}
 
     with open(config_file, "r") as file:
-        data = yaml.load(file, Loader=yaml.SafeLoader)
+        data = yaml.load(file)
     return data
 
    
@@ -118,85 +129,118 @@ def get_or_compute_md5(file_path: str, checksum_log: str) -> str:
 
     return hash_md5
 
-
-def compute_gather(
-        WGS_samples_dir : str,
-        AMP_samples_dir : str,
-        mapping_WGS : str,
-        mapping_AMP : str,
+def compute_gather_amp(
+        AMP_samples_dir: str,
+        mapping_AMP: str,
         metadata_path: str,
-        experiment_type: List[str],
         nested: bool
-        ) -> tuple[pd.DataFrame, str]:
-    
-    # Raise error if samples directory does not exist
-    if WGS_samples_dir and not os.path.exists(WGS_samples_dir):
-        raise FileNotFoundError(f"{WGS_samples_dir} does not exist!")
-    
+) -> tuple[pd.DataFrame, str]:
+
     if AMP_samples_dir and not os.path.exists(AMP_samples_dir):
         raise FileNotFoundError(f"{AMP_samples_dir} does not exist!")
 
-    if experiment_type in ['16S','ITS','18S']:
-        exp_dir = AMP_samples_dir
-        table_mapping = pd.read_csv(mapping_AMP, sep="\t")
-    elif experiment_type == 'WGS':
-        exp_dir = WGS_samples_dir
-        table_mapping = pd.read_csv(mapping_WGS, sep="\t")
-    
-    required_cols = ["forward","reverse","sample_alias"]
+    table_mapping = pd.read_csv(mapping_AMP, sep="\t")
+
+    required_cols = ["r1", "r2", "sample_alias"]
     has_sample_id = "sample_id" in table_mapping.columns
 
     assert all(col in table_mapping.columns for col in required_cols), \
-    f"Logic Error: One or more columns from {required_cols} are missing."
+        f"Logic Error: One or more columns from {required_cols} are missing."
 
     if nested and not has_sample_id:
         raise ValueError("nested_folders=True requires a 'sample_id' column.")
-    
+
     if not nested:
         print('\n[INFO] Assuming read files are in the same directory\n')
-    
     else:
         print('\n[INFO] Sequence files are nested, proceeding to retrieve...\n')
 
     md5_results = []
 
-
     for row in tqdm(table_mapping.itertuples(), total=len(table_mapping), desc="Computing md5sum.."):
-        
-        base_path = os.path.join(exp_dir, str(row.sample_id)) if has_sample_id and nested else exp_dir
+        base_path = os.path.join(AMP_samples_dir, str(row.sample_id)) if has_sample_id and nested else AMP_samples_dir
         if os.path.exists(base_path):
-        
-            r1_path = os.path.join(base_path, row.forward)
-            r2_path = os.path.join(base_path, row.reverse)
-            
-            checksum_file = os.path.join(base_path,'MD5.txt')
+            r1_path = os.path.join(base_path, row.r1)
+            r2_path = os.path.join(base_path, row.r2)
+            checksum_file = os.path.join(base_path, 'MD5.txt')
 
             if os.path.exists(r1_path) and os.path.exists(r2_path):
-
+                h1 = get_or_compute_md5(r1_path, checksum_log=checksum_file)
                 h2 = get_or_compute_md5(r2_path, checksum_log=checksum_file)
-                h1 = get_or_compute_md5(r1_path,checksum_log=checksum_file)
-
                 md5_results.append({
                     "sample_alias": row.sample_alias,
                     "r1_md5sum": h1,
                     "r2_md5sum": h2
                 })
             else:
-                print('Files not found')
+                print(f'[WARN] Files not found for sample: {row.sample_alias}')
 
     md5_df = pd.DataFrame(md5_results)
-
-    if md5_df is not None:
-        updated_table = pd.merge(table_mapping,md5_df ,how='left', on='sample_alias')
+    updated_table = pd.merge(table_mapping, md5_df, how='left', on='sample_alias')
 
     metadata_dir = os.path.dirname(metadata_path)
-    updated_mapping_file = os.path.join(metadata_dir,'mapping_table.tsv')
-    updated_table.to_csv(updated_mapping_file, sep = '\t', index = False)
+    updated_mapping_file = os.path.join(metadata_dir, 'mapping_table_amp.tsv')
+    updated_table.to_csv(updated_mapping_file, sep='\t', index=False)
 
-    print(f'[INFO] updated mapping table saved to: {updated_mapping_file}')
-
+    print(f'[INFO] Updated mapping table saved to: {updated_mapping_file}')
     return updated_table, updated_mapping_file
 
+
+def compute_gather_wgs(
+        WGS_samples_dir: str,
+        mapping_WGS: str,
+        metadata_path: str,
+        nested: bool
+) -> tuple[pd.DataFrame, str]:
+
+    if WGS_samples_dir and not os.path.exists(WGS_samples_dir):
+        raise FileNotFoundError(f"{WGS_samples_dir} does not exist!")
+
+    table_mapping = pd.read_csv(mapping_WGS, sep="\t")
+
+    required_cols = ["r1", "r2", "sample_alias"]
+    has_sample_id = "sample_id" in table_mapping.columns
+
+    assert all(col in table_mapping.columns for col in required_cols), \
+        f"Logic Error: One or more columns from {required_cols} are missing."
+
+    if nested and not has_sample_id:
+        raise ValueError("nested_folders=True requires a 'sample_id' column.")
+
+    if not nested:
+        print('\n[INFO] Assuming read files are in the same directory\n')
+    else:
+        print('\n[INFO] Sequence files are nested, proceeding to retrieve...\n')
+
+    md5_results = []
+
+    for row in tqdm(table_mapping.itertuples(), total=len(table_mapping), desc="Computing md5sum.."):
+        base_path = os.path.join(WGS_samples_dir, str(row.sample_id)) if has_sample_id and nested else WGS_samples_dir
+        if os.path.exists(base_path):
+            r1_path = os.path.join(base_path, row.r1)
+            r2_path = os.path.join(base_path, row.r2)
+            checksum_file = os.path.join(base_path, 'MD5.txt')
+
+            if os.path.exists(r1_path) and os.path.exists(r2_path):
+                h1 = get_or_compute_md5(r1_path, checksum_log=checksum_file)
+                h2 = get_or_compute_md5(r2_path, checksum_log=checksum_file)
+                md5_results.append({
+                    "sample_alias": row.sample_alias,
+                    "r1_md5sum": h1,
+                    "r2_md5sum": h2
+                })
+            else:
+                print(f'[WARN] Files not found for sample: {row.sample_alias}')
+
+    md5_df = pd.DataFrame(md5_results)
+    updated_table = pd.merge(table_mapping, md5_df, how='left', on='sample_alias')
+
+    metadata_dir = os.path.dirname(metadata_path)
+    updated_mapping_file = os.path.join(metadata_dir, 'mapping_table_wgs.tsv')
+    updated_table.to_csv(updated_mapping_file, sep='\t', index=False)
+
+    print(f'[INFO] Updated mapping table saved to: {updated_mapping_file}')
+    return updated_table, updated_mapping_file
 
 
 def create_run(
@@ -235,7 +279,7 @@ def create_run(
         table_mapping = mapping
     
     run_xml = []
-    required_cols = ["forward", "reverse","sample_alias","r1_md5sum","r2_md5sum"]
+    required_cols = ["r1", "r2","sample_alias","r1_md5sum","r2_md5sum"]
     
     assert all(col in table_mapping.columns for col in required_cols), \
     f"Logic Error: One or more columns from {required_cols} are missing."
@@ -253,9 +297,9 @@ def create_run(
         exp_alias = f"{project_name}-{sample_alias}-{experiment_type}"
         template_xml = template_xml\
             .replace("$$$EXPERIMENT_ALIAS$$$",  exp_alias)\
-            .replace("$$$FORWARD_R1_FASTQ$$$",  row.forward)\
+            .replace("$$$FORWARD_R1_FASTQ$$$",  row.r1)\
             .replace("$$$FORWARD_R1_MD5SUM$$$", row.r1_md5sum)\
-            .replace("$$$REVERSE_R2_FASTQ$$$",  row.reverse)\
+            .replace("$$$REVERSE_R2_FASTQ$$$",  row.r2)\
             .replace("$$$REVERSE_R2_MD5SUM$$$", row.r2_md5sum)
         run_xml += [template_xml]
 
@@ -281,11 +325,13 @@ def parse_args():
     parser = argparse.ArgumentParser("preprocess_sequences")
     parser.add_argument("-s", "--config_path", 
                         help="config yaml file containing direcotries for the whole workflow.",
+                        required=True,
                         type=str
                         )
     parser.add_argument("-e", "--experiment_types",
                         help="String defining either 16S or WGS",
-                        choices=["AMP", "WGS"]    
+                        required=True,
+                        choices=["16S", "WGS"]    
                         )
     parser.add_argument("-n", "--nested",
                         help="If sequences files are nested within each corrispective sample dir names",
